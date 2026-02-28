7a9e80a0d45f670d3475df014a83f41cf5dc87e3
frayd <>
2026-02-28 16:38:51 -0500

    Remove all the unused code from stroke engine to make it easier to fully integrate in the future

--- Software/lib/StrokeEngine/src/StrokeEngine.h
+++ Software/lib/StrokeEngine/src/StrokeEngine.h
@@ -61,31 +61,14 @@ typedef struct {
 
 /**************************************************************************/
 /*!
-  @brief  Struct defining the endstop properties like pin, pinmode, polarity
-  and homing direction.
-*/
-/**************************************************************************/
-typedef struct {
-    bool homeToBack; /*> Set to true to home to the back of the machine
-                      *  Set to false to home to the front of the machine */
-    bool activeLow;  /*> Polarity of the homing signal. True for active low. */
-    int endstopPin;  /*> Pin connected to home switch */
-    uint8_t pinMode; /*> Pinmode of the switch INPUT, INPUT_PULLUP,
-                        INPUT_PULLDOWN */
-} endstopProperties;
-
-/**************************************************************************/
-/*!
   @brief  Enum containing the states of the state machine
 */
 /**************************************************************************/
 typedef enum {
     UNDEFINED,   //!< No power to the servo. We don't know its position
     READY,       //!< Servo is energized and knows it position. Not running.
-    PATTERN,     //!< Stroke Engine is running and servo is moving according to
+    PATTERN      //!< Stroke Engine is running and servo is moving according to
                  //!< defined pattern.
-    SETUPDEPTH,  //!< Interactive adjustment mode to setup depth and stroke
-    STREAMING    //!< Tracks the depth-position whenever depth is updated.
 } ServoState;
 
 // Verbose strings of states for debugging purposes
@@ -129,15 +112,6 @@ class StrokeEngine {
 
     /**************************************************************************/
     /*!
-      @brief  Get the speed of a stroke. Speed is returned as Strokes per
-      Minute.
-      @return Strokes per Minute.
-    */
-    /**************************************************************************/
-    float getSpeed();
-
-    /**************************************************************************/
-    /*!
       @brief  Set the depth of a stroke. Settings tale effect with next stroke,
       or after calling applyNewSettingsNow().
       @param depth Depth in [mm]. Is constrained from 0 to TRAVEL
@@ -148,14 +122,6 @@ class StrokeEngine {
 
     /**************************************************************************/
     /*!
-      @brief  Returns the depth of a stroke.
-      @return Depth in [mm]. Is constrained from 0 to TRAVEL
-    */
-    /**************************************************************************/
-    float getDepth();
-
-    /**************************************************************************/
-    /*!
       @brief  Set the stroke length of a stroke. Settings take effect with next
       stroke, or after calling applyNewSettingsNow().
       @param stroke Stroke length in [mm]. Is constrained from 0 to TRAVEL
@@ -163,14 +129,6 @@ class StrokeEngine {
     */
     /**************************************************************************/
     void setStroke(float stroke, bool applyNow);
-
-    /**************************************************************************/
-    /*!
-      @brief  Get the stroke length of a stroke.
-      @return Stroke length in [mm].
-    */
-    /**************************************************************************/
-    float getStroke();
 
     /**************************************************************************/
     /*!
@@ -186,16 +144,6 @@ class StrokeEngine {
 
     /**************************************************************************/
     /*!
-      @brief  Get the sensation of a pattern. Sensation is an additional
-      parameter a pattern may use to alter its behaviour.
-      @return Sensation in [a.u.]. Is constrained from -100 to 100
-                    with 0 beeing assumed as neutral.
-    */
-    /**************************************************************************/
-    float getSensation();
-
-    /**************************************************************************/
-    /*!
       @brief  Choose a pattern for the StrokeEngine. Settings take effect with
       next stroke, or after calling applyNewSettingsNow().
       @param patternIndex  Index of a pattern
@@ -205,14 +153,6 @@ class StrokeEngine {
     */
     /**************************************************************************/
     bool setPattern(Pattern *nextPattern, bool applyNow);
-
-    /**************************************************************************/
-    /*!
-      @brief  Get the pattern index for the StrokeEngine.
-      @return Index of a pattern
-    */
-    /**************************************************************************/
-    int getPattern();
 
     /**************************************************************************/
     /*!
@@ -234,28 +174,6 @@ class StrokeEngine {
 
     /**************************************************************************/
     /*!
-      @brief  Enable the servo/stepper and do the homing procedure. Drives
-      towards the endstop with HOMING_SPEED. Function is non-blocking and backed
-      by a task. Optionally a callback can be given to receive feedback if
-      homing succeeded going in state READY. If homing switch is not found after
-      traveling MAX_TRAVEL it times out, disables the servo and goes into
-      UNDEFINED.
-      @param endstop Pointer to a endstopProperties struct defining all relevant
-                    properties like pin, pinmode, homing direction & signal
-                    polarity.
-      @param speed  Speed in mm/s used for finding the homing switch.
-                    Defaults to 5.0 mm/s
-      @param callBackHoming Callback function is called after homing is done.
-                    Function parametere holds a bool containing the success
-      (TRUE) or failure (FALSE) of homing.
-    */
-    /**************************************************************************/
-    void enableAndHome(endstopProperties *endstop, float speed = 5.0);
-    void enableAndHome(endstopProperties *endstop, void (*callBackHoming)(bool),
-                       float speed = 5.0);
-
-    /**************************************************************************/
-    /*!
       @brief  If no homing switch is present homing can be done manually. Push
       the endeffector all the way in and call thisIsHome(). This enables the
       the servo and sets the position to -KEEPOUT_BOUNDARY
@@ -267,114 +185,11 @@ class StrokeEngine {
 
     /**************************************************************************/
     /*!
-      @brief  In state PATTERN, SETUPDEPTH and READY this
-      moves the endeffector to TRAVEL. Can be used for adjustments. Stops any
-      running pattern and ends in state READY.
-      @param speed  Speed in mm/s used for driving to max.
-                    Defaults to 10.0 mm/s
-      @return TRUE on success, FALSE if state does not allow this.
-    */
-    /**************************************************************************/
-    bool moveToMax(float speed = 10.0);
-
-    /**************************************************************************/
-    /*!
-      @brief  In state PATTERN, SETUPDEPTH and READY this
-      moves the endeffector to 0. Can be used for adjustments. Stops any running
-      pattern and ends in state READY.
-      @param speed  Speed in mm/s used for driving to min.
-                    Defaults to 10.0 mm/s
-      @return TRUE on success, FALSE if state does not allow this.
-    */
-    /**************************************************************************/
-    bool moveToMin(float speed = 10.0);
-
-    /**************************************************************************/
-    /*!
-      @brief  In state PATTERN and READY this moves the endeffector
-      to DEPTH and enters state SETUPDEPTH. Follows the DEPTH postion
-      whenever setDepth() is called. Can be used for adjustments. Stops any
-      running pattern.
-      @param speed  Speed in mm/s used for driving to min.
-                    Defaults to 10.0 mm/s
-      @param fancy  In fancy mode sensation allows to adjust both, depth and
-                    stroke. +100 adjusts the depth position, -100 adjusts the
-                    stroke position. 0 adjusts the midpoint depth-stroke/2.
-      @return TRUE on success, FALSE if state does not allow this.
-    */
-    /**************************************************************************/
-    bool setupDepth(float speed = 10.0, bool fancy = false);
-
-    /**************************************************************************/
-    /*!
       @brief  Retrieves the current servo state from the internal state machine.
       @return Current state of the state machine
     */
     /**************************************************************************/
     ServoState getState();
-
-    /**************************************************************************/
-    /*!
-      @brief  Disables the servo motor instantly and deletes any motion task.
-      Sets state machine to UNDEFINED. Must be followed by homing to enable
-      servo again.
-    */
-    /**************************************************************************/
-    void disable();
-
-    /**************************************************************************/
-    /*!
-      @brief  Makes the pattern list available for the main program to retreive
-      informations like pattern names.
-      @param index index of a pattern.
-      @return String holding a pattern name with a certain index. If index is
-                    out of range it returns "Invalid"
-    */
-    /**************************************************************************/
-    String getPatternName(int index);
-
-    /**************************************************************************/
-    /*!
-      @brief  Makes the pattern list available for the main program to retreive
-      informations like pattern names.
-      @return The number of pattern available.
-    */
-    /**************************************************************************/
-    unsigned int getNumberOfPattern() { return 0; };
-
-    /**************************************************************************/
-    /*!
-      @brief  Updates the maximum speed number of StrokeEngine. This value is
-      used to keep alle motions in check and as a safeguard.
-      @param maxSpeed maximum Speed in mm/s
-    */
-    /**************************************************************************/
-    void setMaxSpeed(float maxSpeed);
-
-    /**************************************************************************/
-    /*!
-      @brief  Get the current set maximum speed
-      @return maximum speed in mm/s
-    */
-    /**************************************************************************/
-    float getMaxSpeed();
-
-    /**************************************************************************/
-    /*!
-      @brief   Updates the maximum acceleration number of StrokeEngine. This
-      value is used to keep alle motions in check and as a safeguard.
-      @param maxAcceleration maximum acceleration in mm/s²
-    */
-    /**************************************************************************/
-    void setMaxAcceleration(float maxAcceleration);
-
-    /**************************************************************************/
-    /*!
-      @brief  Get the current set maximum acceleration
-      @return maximum acceleration in mm/s²
-    */
-    /**************************************************************************/
-    float getMaxAcceleration();
 
     /**************************************************************************/
     /*!
@@ -409,18 +224,10 @@ class StrokeEngine {
     float _timeOfStroke;
     float _sensation;
     bool _applyUpdate = false;
-    static void _homingProcedureImpl(void *_this) {
-        static_cast<StrokeEngine *>(_this)->_homingProcedure();
-    }
-    void _homingProcedure();
     static void _strokingImpl(void *_this) {
         static_cast<StrokeEngine *>(_this)->_stroking();
     }
     void _stroking();
-    static void _streamingImpl(void *_this) {
-        static_cast<StrokeEngine *>(_this)->_streaming();
-    }
-    void _streaming();
     TaskHandle_t _taskStrokingHandle = NULL;
     TaskHandle_t _taskHomingHandle = NULL;
     TaskHandle_t _taskStreamingHandle = NULL;
@@ -433,5 +240,4 @@ class StrokeEngine {
     int _homeingToBack;
     bool _homeingActiveLow; /*> Polarity of the homing signal*/
     bool _fancyAdjustment;
-    void _setupDepths();
 };
